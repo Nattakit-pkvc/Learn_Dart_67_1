9873bbf8065e00098ec890a23c66824e287d0704
Nattakit-pkvc <6749010031@phuketvc.ac.th>
2025-08-02 15:13:21 +0700

    Loop

---
+++ dart16.dart
@@ -0,0 +1,40 @@
+// Loop
+
+void main() {
+  // For loop
+  for (int i = 0; i < 5; i++) {
+    print(i);
+  }
+  print("-----------------------");
+
+  // For .. in loop
+  var numbers = [1, 2, 3, 4, 5];
+  for (var number in numbers) {
+    print('For .. in loop number: $number');
+  }
+  print("-----------------------");
+
+  // For each loop
+  var numbers2 = [1, 2, 3, 4, 5];
+  numbers2.forEach((number) {
+    print('For each loop number: $number');
+  });
+  print("-----------------------");
+
+  // While loop
+  var num_loop = 5;
+  int j = 0;
+  while (j < num_loop) {
+    print('While loop iteratiom: $j');
+    j++;
+  }
+  print("-----------------------");
+
+  // Do while loop
+  var num_loop2 = 5;
+  int k = 0;
+  do {
+    print('Do While Loop iteration:$k');
+    k++;
+  } while (k < num_loop2);
+}
